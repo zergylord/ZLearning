62e050cbc9f9f77c03f0bdbf53240dc8fe0b2836
Steven <steven@osaro.com>
2017-08-01 18:40:55 -0700

    z learning control works with learning inverse dynamics

---
+++ control_z.py
@@ -0,0 +1,71 @@
+from utils import *
+import matplotlib.pyplot as plt
+import numpy as np
+from environment import ContGrid
+def z_iteration(Theta,R,Z):
+    return np.matmul(np.matmul(np.diag(np.exp(R)),Theta),Z)
+def sample_passive():
+    return np.random.randn(2)*.01
+env = ContGrid()
+s_dim = 2
+a_dim = 2
+RP = np.random.randn(s_dim,100)
+oracle_id = False
+b = .01
+n_mem = 1000
+S = np.zeros([n_mem,s_dim])
+A = np.zeros([n_mem,a_dim])
+R = np.zeros([n_mem])
+term = np.zeros([n_mem],dtype=np.int32)
+SPrime = np.random.randn(n_mem,s_dim)
+#uniform sampling
+for i in range(n_mem):
+    S[i,:] = env.observation_space.sample()
+    A[i,:] = sample_passive()
+    SPrime[i,:],R[i],term[i],_ = env.get_transition(S[i,:],A[i,:])
+n_term = term.sum()
+Theta = np.zeros([n_mem,n_mem+n_term])
+Theta[:,:-n_term] = rbf(SPrime,S,b)
+term_R = np.zeros([n_term,1])
+for i,ind in enumerate(np.nonzero(term)[0]):
+    Theta[ind,:] = 0
+    Theta[ind,-n_term+i] = 1.0
+    term_R[i] = R[ind]
+NN = Theta[:,:-n_term]
+NT = Theta[:,-n_term:]
+print(NN.shape,NT.shape)
+M = np.diag(np.exp(R))
+LHS = np.eye(n_mem)-np.matmul(M,NN)
+RHS = np.matmul(np.matmul(M,NT),np.exp(term_R))
+Z = np.linalg.solve(LHS,RHS)
+plt.scatter(SPrime[:,0],SPrime[:,1],c=np.log(Z[:,0]))
+plt.colorbar()
+plt.show()
+
+s = np.asarray([.9,.9])#env.observation_space.sample()
+plt.ion()
+for i in range(1000):
+    #P(s'|s)
+    theta = rbf(np.expand_dims(s,0),S,b)
+    part = np.dot(theta[0],Z[:,0])
+    control = theta[0]*Z[:,0]/part
+    if i % 10 == 0:
+        plt.clf()
+        plt.scatter(SPrime[:,0],SPrime[:,1],c=control)
+        plt.colorbar()
+        #plt.show()
+        plt.pause(.001)
+    ind = np.random.choice(n_mem,p=control)
+    if oracle_id:
+        #ind = np.argmax(control)
+        s = SPrime[ind]
+    else:
+        id_theta = rbf(np.matmul(SPrime,RP)-np.matmul(np.expand_dims(s,0),RP),
+                np.matmul(SPrime,RP)-np.matmul(S,RP),b)
+        pred_A = np.matmul(id_theta,A)
+        a = pred_A[ind]
+        sPrime,r,term,_ = env.get_transition(s,a)
+        #print(s,a,sPrime)
+        s = sPrime.copy()
+
+#sPrime,r,term,_ = env.get_transition(s,a)
--- environment.py
+++ environment.py
@@ -21,13 +21,13 @@ class ContGrid():
         return self.s
     def get_transition(self,s,a):
         term = False
-        r = -.1
+        r = -1.0
         sPrime =  s + self.a_scale*a #+ np.random.randn(2)*self.a_scale/1000
         if np.any(sPrime > self.max_s) or np.any(sPrime < 0):
             sPrime[sPrime >= self.max_s] = self.max_s-.01*self.max_s
             sPrime[sPrime < 0] = 0+.01*self.max_s
             r = -1.0
-            term = True
+            #term = True
         elif np.all(sPrime > self.goal) and np.all(sPrime < (self.goal+self.goal_size)):
             r = 0
             term = True
